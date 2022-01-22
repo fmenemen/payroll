5d36d9355f23eec2564ffdabbda829fd52a149b9
Fotis Menemenis <foumou@Fotiss-MacBook-Air.local>
2022-01-22 10:36:23 +0100

    Remove unused controller and target folder

--- target/flow-frontend/OfflineStub.d.ts
+++
@@ -1,5 +0,0 @@
-import { LitElement } from 'lit-element';
-export declare class OfflineStub extends LitElement {
-    static get styles(): import("lit-element").CSSResult;
-    render(): import("lit-element").TemplateResult;
-}
